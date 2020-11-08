0bdf3e6827c0212bcd96ca54fb056bd06ca5b5b5
Falahuddin <falahuddinas@gmail.com>
2020-11-09 03:44:32 +0800

    fix hash router

--- src/containers/route.js
+++ src/containers/route.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter, Route, Redirect } from "react-router-dom";
 import Cookies from "js-cookie";
 import Login from "./pages/auth/login/login";
 import Home from "./pages/home/home";
@@ -16,22 +16,18 @@ export default function RouteControl() {
   };
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Route
-          exact
-          path="/login"
-          render={() => (isLogin() ? <Redirect to="/home" /> : <Login />)}
-        />
-        <Route
-          exact
-          path="/home"
-          render={() => (isLogin() ? <Home /> : <Redirect to="/login" />)}
-        />
-        <Route path="/login" render={() => <Login />} />
-        <Route path="/home" render={() => <Home />} />
-      </Switch>
-    </BrowserRouter>
+    <HashRouter basename="/#">
+      <Route exact path="/" render={() => <Redirect to="/home" />} />
+      <Route
+        exact
+        path="/login"
+        render={() => (isLogin() ? <Redirect to="/home" /> : <Login />)}
+      />
+      <Route
+        exact
+        path="/home"
+        render={() => (isLogin() ? <Home /> : <Redirect to="/login" />)}
+      />
+    </HashRouter>
   );
 }
